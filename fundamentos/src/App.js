8e3ff57770251ca769f5055b93b189a0544a648a
Eike Matos <eikeadrian9830@gmail.com>
2021-06-23 22:25:23 -0300

    Block 1

--- fundamentos/src/App.js
+++ fundamentos/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 
-import CompPadrao, { Comp1, Comp2} from './components/Multi'
-import Primeiro from './components/Primeiro'
+import MinMax from './components/MinMax'
+// import CompPadrao, { Comp1, Comp2} from './components/Multi'
+// import Primeiro from './components/Primeiro'
 
 export default () => ( 
-        <SafeAreaView>
-            <CompPadrao/>
+        <SafeAreaView style={style.App}>
+            <MinMax min={3} max={20} />
+            <MinMax min={1} max={94} />
+            {/*<CompPadrao/>
             <Comp1 />
             <Comp2 />
-            <Primeiro/ >
+            <Primeiro/ > */}
         </SafeAreaView>
 )
+
+const style = StyleSheet.create( {
+    App: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
+
+    }
+})
--- fundamentos/src/components/Multi.js
+++ fundamentos/src/components/Multi.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Estilo from './estilo'
 
 export default function Comp() {
-    return <Text> Comp #Oficial</Text>
+    return <Text style ={Estilo.txtG}> Comp #Oficial</Text>
 }
 export function Comp1() {
-    return <Text> Comp #O1</Text>
+    return <Text style={Estilo.txtG}> Comp #O1</Text>
 }
 export function Comp2() {
-    return <Text> Comp #O2</Text>
+    return <Text style={Estilo.txtG}> Comp #O2</Text>
 }
 
 export { Comp1, Comp2}
