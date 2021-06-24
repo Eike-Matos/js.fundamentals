8419f9d8ae74a932ce362c77a7e2f9396c90b18f
Eike Matos <eikeadrian9830@gmail.com>
2021-06-23 21:23:06 -0300

    Initiated

---
+++ fundamentos/src/components/Multi.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Comp() {
+    return <Text> Comp #Oficial</Text>
+}
+export function Comp1() {
+    return <Text> Comp #O1</Text>
+}
+export function Comp2() {
+    return <Text> Comp #O2</Text>
+}
+
+export { Comp1, Comp2}
